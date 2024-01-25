bb10ad2fd9745af987e3abe004bd4daece1107f3
Orlando Chavez <orlando@sidmac.com>
2024-01-25 12:29:23 -0600

    25012024 07

--- resources/views/layouts/firmaElectronica/contratohasimilados.blade.php
+++ resources/views/layouts/firmaElectronica/contratohasimilados.blade.php
@@ -69,8 +69,7 @@
 </head>
     <body>
         <footer>
-            {{-- <div style="display: inline-block; width: 50%;"></div> --}}
-            <div class="page-number"><small class="link">Sello Digital: | GUID: {{$uuid}} | Sello: {{$cadena_sello}} | Fecha: {{$fecha_sello}} <br> Este documento ha sido Firmado Electrónicamente, teniendo el mismo valor que la firma autógrafa de acuerdo a los Artículos 1, 3, 8 y 11 de la Ley de Firma Electrónica Avanzada del Estado de Chiapas </small></div>
+            <div class="page-number"></div>
         </footer>
         <div class= "container g-pt-30" style="font-size: 12px; margin-bottom: 25px;" >
             <div id="content">
