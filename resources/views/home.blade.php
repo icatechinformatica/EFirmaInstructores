d10c536824301de0cc5104387916d5b9a28b808c
Jose-Wicho <luisito08672@gmail.com>
2024-02-07 09:52:44 -0600

    Eliminar logo efirma instructores

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,12 @@
 
 @section('css')
     <style>
-        .colorTop { 
+        .colorTop {
             background-color: #541533;
+        }
+        .colorTexto {
+            color: #541533;
+            font-size: 50px;
         }
     </style>
 @endsection
@@ -16,7 +20,8 @@
         <div class="row justify-content-center pt-5">
 
             <div class="col-12 d-flex justify-content-center">
-                <img src="{{ asset('img/icatech-imagen.png') }}" class="img-fluid">
+                {{-- <img src="{{ asset('img/icatech-imagen.png') }}" class="img-fluid"> --}}
+                <span class="colorTexto font-weight-bold">EFIRMA</span>
             </div>
 
             <div class="col-12 d-flex justify-content-center mt-4">
