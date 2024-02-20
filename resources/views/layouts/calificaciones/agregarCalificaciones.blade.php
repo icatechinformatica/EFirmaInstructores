fd8fae3679e7acd4417b6581484ca894cab6c4e5
Orlando Chavez <orlando@sidmac.com>
2024-02-20 11:28:03 -0600

    20022024 04

--- resources/views/layouts/calificaciones/agregarCalificaciones.blade.php
+++ resources/views/layouts/calificaciones/agregarCalificaciones.blade.php
@@ -106,7 +106,7 @@
                                                         {{ 'NINGUNO' }} @endif
                                                 </td>
                                                 <td>
-                                                    @if ((!$a->folio or $a->folio == '0') && ($a->porcentaje_asis >= 70 && !is_null($a->porcentaje_asis)))
+                                                    @if ((!$a->folio or $a->folio == '0') && $a->porcentaje_asis >= 70 && $a->porcentaje_asis != NULL)
                                                         <?php $cambios = true; ?>
                                                         {{ Form::text('calificacion[' . $a->id . ']', $a->calificacion, ['id' => $a->id, 'class' => 'form-control numero', 'required' => 'required', 'size' => 1]) }}
                                                     @else
