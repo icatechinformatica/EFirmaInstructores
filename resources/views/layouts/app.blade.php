548d8c268b2b9f76fb7cdafbd21da15c9a9f8ad7
Jose-Wicho <luisito08672@gmail.com>
2024-04-04 12:19:59 -0600

    Quitar inicio de sesion y registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,14 +40,14 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Inicio de Sesión') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registro') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
