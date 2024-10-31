dea0bf7df3940607444299de85fd711489e4c83a
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2024-10-31 14:28:16 -0600

    ACTUALIZACION

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,14 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    {/*
         if ($this->app->environment('production')) {
             # forzamos el esquema a trabajar con https
             URL::forceScheme('https');
-        }   
+        }   */
+
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
